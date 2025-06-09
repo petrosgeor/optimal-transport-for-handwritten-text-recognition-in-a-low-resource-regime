8185e5817fd1620aa3a63851bd6f4b548125de45
Petros Georgoulas <petrosgw00@gmail.com>
2025-06-09 13:46:39 +0300

    center images in he preprocess function

--- htr_base/utils/preprocessing.py
+++ htr_base/utils/preprocessing.py
@@ -20,21 +20,24 @@ def load_image(image_path):
 
 
 def preprocess(img, input_size, border_size=8):
-    
+    """Resize ``img`` and symmetrically pad it to ``input_size``."""
+
     h_target, w_target = input_size
 
     n_height = min(h_target - 2 * border_size, img.shape[0])
-    
+
     scale = n_height / img.shape[0]
     n_width = min(w_target - 2 * border_size, int(scale * img.shape[1]))
 
     img = resize(image=img, output_shape=(n_height, n_width)).astype(np.float32)
 
-    # right pad image to input_size
-    img  = np.pad(img, ((border_size, h_target - n_height - border_size), (border_size, w_target - n_width - border_size),),
-                                                    mode='median')
+    # symmetric padding to input_size
+    pad_v_extra = h_target - n_height - 2 * border_size
+    pad_h_extra = w_target - n_width - 2 * border_size
+    top = border_size + pad_v_extra // 2
+    bottom = border_size + pad_v_extra - pad_v_extra // 2
+    left = border_size + pad_h_extra // 2
+    right = border_size + pad_h_extra - pad_h_extra // 2
+    img = np.pad(img, ((top, bottom), (left, right)), mode='median')
 
     return img
-
-
-
